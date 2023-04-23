ae304c5b7b0e0970e24e64eba56f2ea6a11bd2d8
dm2x <dm2xme@gmail.com>
2023-04-23 13:04:33 +0100

    some work done

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddias-ma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/15 16:09:50 by ddias-ma          #+#    #+#             */
-/*   Updated: 2023/04/15 18:06:30 by ddias-ma         ###   ########.fr       */
+/*   Created: 2023/04/15 18:10:07 by ddias-ma          #+#    #+#             */
+/*   Updated: 2023/04/23 13:03:35 by ddias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lstsize(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
-		return (0);
-	else
-		return (1 + ft_lstsize(lst -> next));
+		return (NULL);
+	while (lst -> next != NULL)
+		lst = lst -> next;
+	return (lst);
 }
